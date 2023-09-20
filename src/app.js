48965bfd0fbfda0d1c0256bb29fa1c5ae22ac2f1
unknown <caestrada769@misena.edu.co>
2023-09-19 19:06:18 -0500

    tercer commit

--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
-const categoryRoutes = require('./routes/categoryRoutes');
+const productionRoutes = require('./routes/productionRoutes');
 
 app.use(bodyParser.json());
 
 // Conectar las rutas
 app.use('/api', userRoutes);
-app.use('/api', categoryRoutes);
+app.use('/api', productionRoutes);
 
 module.exports = app;
